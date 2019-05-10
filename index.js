3a6052256a1df5d57c795322528396935bb13e2d
Baheem Ferrell <baheemferrell.student@careerdevs.com>
2019-05-10 00:53:06 +0000

    handling Request

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const express = require('express');
 //set up express app
 const app = express();
 
-//listen for requests
-app.listen(process.env.port || 4000, function(){
-  
-  console.log('now listening for reuqests');
+app.get('/api', function(req, res){
+  console.log('GET request');
+  res.send({name: 'Yoshi'});
 });
+
+
+const port = process.env.PORT;
+app.listen(port, ()=>console.log('now listening for reuqests'));
+
+//listen for requests
+//app.listen(process.env.port || 4000, function(){
+//  
+//  console.log('now listening for reuqests');
+//});
