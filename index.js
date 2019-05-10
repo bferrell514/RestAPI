d68fc1ad1e6242a5841fff2fd5eab5d8ae85a7a7
Baheem Ferrell <baheemferrell.student@careerdevs.com>
2019-05-10 01:24:05 +0000

    creating routes

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require('express');
-
+const routes = require('./routes/api')
 //set up express app
 const app = express();
 
-app.get('/api', function(req, res){
-  console.log('GET request');
-  res.send({name: 'Yoshi'});
-});
-
-
-const port = process.env.PORT;
-app.listen(port, ()=>console.log('now listening for reuqests'));
+app.use('/api',routes);
 
 //listen for requests
-//app.listen(process.env.port || 4000, function(){
-//  
-//  console.log('now listening for reuqests');
-//});
+const port = process.env.PORT;
+app.listen(port, ()=>console.log('now listening for reuqests'));
