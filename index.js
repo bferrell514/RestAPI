863123921daecec0740478d0184b9da499e05104
Baheem Ferrell <baheemferrell.student@careerdevs.com>
2019-05-11 23:20:34 +0000

    setting up express

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-//set up express app
+// set up express app
 const app = express();
 
-//listen for requests
+app.get('/api', function(req, res){
+  res.send({ name: 'Yoshi' });
+});
+
+// listen for requests
 app.listen(process.env.port || 4000, function(){
-  
-  console.log('now listening for reuqests');
+    console.log('now listening for requests');
 });
